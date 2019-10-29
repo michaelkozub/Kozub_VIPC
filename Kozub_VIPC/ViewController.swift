22dc1522044d010f5c085e1ebebdf8b0116c345f
Michael Kozub <michaelkozub@hotmail.com>
2019-10-29 16:01:59 -0600

    VIPC file stubs

--- Kozub_VIPC/ViewController.swift
+++ Kozub_VIPC/ViewController.swift
@@ -1,11 +1,3 @@
-//
-//  ViewController.swift
-//  Kozub_VIPC
-//
-//  Created by Michael Kozub on 10/29/19.
-//  Copyright © 2019 Michael Kozub. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
